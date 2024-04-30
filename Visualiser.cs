b82f9b0c5875d79cbbf79e4537a469f51b732ae6
tairasoul <100938045+tairasoul@users.noreply.github.com>
2024-04-30 14:44:35 +0200

    ballin

---
+++ Visualiser.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+namespace BoundsMaker
+{
+    internal class VisualiserComponent : MonoBehaviour
+    {
+        internal Bounds bounds;
+        private void Update()
+        {
+            VisualizeBound(bounds);
+        }
+
+        private void VisualizeBound(Bounds bounds)
+        {
+            LineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
+            if (!lineRenderer)
+            {
+                lineRenderer = gameObject.AddComponent<LineRenderer>();
+                // Set LineRenderer properties (same as in your code)
+            }
+
+            Vector3 center = bounds.center;
+            Vector3 size = bounds.size;
+
+            Vector3[] corners =
+            {
+                center + new Vector3(size.x, size.y, size.z) * 0.5f,
+                center + new Vector3(size.x, size.y, -size.z) * 0.5f,
+                center + new Vector3(-size.x, size.y, -size.z) * 0.5f,
+                center + new Vector3(-size.x, size.y, size.z) * 0.5f,
+                center + new Vector3(size.x, -size.y, size.z) * 0.5f,
+                center + new Vector3(size.x, -size.y, -size.z) * 0.5f,
+                center + new Vector3(-size.x, -size.y, -size.z) * 0.5f,
+                center + new Vector3(-size.x, -size.y, size.z) * 0.5f
+            };
+
+            // Define indices to draw the edges of the box
+            int[] indices =
+            {
+                0, 1, 1, 2, 2, 3, 3, 0, // Top face
+                4, 5, 5, 6, 6, 7, 7, 4, // Bottom face
+                0, 4, 1, 5, 2, 6, 3, 7  // Vertical edges
+            };
+
+            lineRenderer.positionCount = indices.Length;
+
+            for (int i = 0; i < indices.Length; i++)
+            {
+                lineRenderer.SetPosition(i, corners[indices[i]]);
+            }
+        }
+    }
+}
